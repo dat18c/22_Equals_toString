1c14c761d41f63a1e649d7d473520f58abd2d50b
JarlTuxen <jart@kea.dk>
2018-10-01 14:55:13 +0200

    tilføjet skole

---
+++ kode/Skole.java
@@ -0,0 +1,19 @@
+public class Skole
+{
+   
+   public static void main(String[] args)
+   {
+      //lav 3 studerende
+      
+      //sæt 3 studerendes højde og vægt
+      
+      //beregn bmi
+      
+      //find ud af om 113 og 117 er primtal
+      
+      //udskriv vha toString
+      
+      //sammenlign studerende med equals
+   
+   }
+}
